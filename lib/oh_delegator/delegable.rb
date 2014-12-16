d26e9d263baa5c7661f5c866526fdb59bb4413c9
Alex Johnson <notalexjohnson@gmail.com>
2014-12-16 08:55:42 +0530

    Allow any delegator name pattern

--- lib/oh_delegator/delegable.rb
+++ lib/oh_delegator/delegable.rb
@@ -2,13 +2,13 @@ class OhDelegator
   module Delegable
     def assign_delegators(*fields)
       fields.each do |field|
-        define_method "#{ field }_delegator" do
-          instance_variable_name = "@#{ field }_delegator"
+        define_method field do
+          instance_variable_name = "@#{ field }"
           instance_variable = instance_variable_get(instance_variable_name)
 
           return instance_variable if instance_variable
 
-          klass = "#{ self.class.name }::#{ field.to_s.classify }Delegator".constantize
+          klass = "#{ self.class.name }::#{ field.to_s.classify }".constantize
           instance_variable_set(instance_variable_name, klass.new(self))
         end
       end
