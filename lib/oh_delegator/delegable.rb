b5f00a418a92b1dd345c1423a86b0a202bb9d427
Alex Johnson <notalexjohnson@gmail.com>
2014-12-16 12:28:15 +0530

    Always load delegator classes alongwith the delegable
    Load the delegator defined relationships and validations when the
    delegable is loaded.

--- lib/oh_delegator/delegable.rb
+++ lib/oh_delegator/delegable.rb
@@ -2,13 +2,15 @@ module OhDelegator
   module Delegable
     def oh_delegators(*attributes)
       attributes.each do |attribute|
+        # Read the delegator class alongwith the delegable.
+        klass = "#{ name }::#{ attribute.to_s.classify }".constantize
+
         define_method attribute do
           instance_variable_name = "@#{ attribute }"
           instance_variable = instance_variable_get(instance_variable_name)
 
           return instance_variable if instance_variable
 
-          klass = "#{ self.class.name }::#{ attribute.to_s.classify }".constantize
           instance_variable_set(instance_variable_name, klass.new(self))
         end
       end
