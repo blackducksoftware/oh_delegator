cc95a8b250e00872e62dd6103be6b90a79dbd03e
Alex Johnson <notalexjohnson@gmail.com>
2014-12-18 00:56:41 +0530

    Extract parent_scope logic into separate module

--- lib/oh_delegator.rb
+++ lib/oh_delegator.rb
@@ -1,3 +1,4 @@
+require 'oh_delegator/parent_scope'
 require 'oh_delegator/base'
 require 'oh_delegator/delegable'
 require 'oh_delegator/orm'
--- lib/oh_delegator/base.rb
+++ lib/oh_delegator/base.rb
@@ -1,19 +1,5 @@
 module OhDelegator
   class Base < SimpleDelegator
-    class << self
-      def parent_scope(&block)
-        delegable.class_exec(&block)
-      end
-
-      def delegable_name
-        @delegable_name ||= name.slice(/^[^:]+/)
-      end
-
-      private
-
-      def delegable
-        @delegable ||= delegable_name.constantize
-      end
-    end
+    extend ParentScope
   end
 end
