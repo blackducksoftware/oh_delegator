fee519fe370641a234388faf9c36a3db3c32b85d
Alex Johnson <notalexjohnson@gmail.com>
2015-09-10 22:54:43 +0530

    Using parent method to simplify parent_scope implementation

--- lib/oh_delegator/parent_scope.rb
+++ lib/oh_delegator/parent_scope.rb
@@ -1,17 +1,7 @@
 module OhDelegator
   module ParentScope
     def parent_scope(&block)
-      delegable.class_exec(&block)
-    end
-
-    private
-
-    def delegable
-      @delegable ||= delegable_name.constantize
-    end
-
-    def delegable_name
-      @delegable_name ||= name.slice(/^[^:]+/)
+      parent.class_exec(&block)
     end
   end
 end
